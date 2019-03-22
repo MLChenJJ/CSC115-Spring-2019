58700b8cd6da43476aad009cb29f3db204da9bbc
shiyi <cmlcug@gmail.com>
2019-03-22 10:53:54 -0700

    Add files via upload

---
+++ Assignment4/source file/a4_files/MazeSolver.java
@@ -0,0 +1,23 @@
+/*
+ * MazeSolver.java
+ *
+ * UVic CSC 115, Spring 2017
+ *
+ * Purpose:
+ *   class that contains a single public static method called
+ *   "findPath". To involve the method one must have already created
+ *   an instance of Maze. The method must return a path through the
+ *   maze (if it exists) in the format shown within the Assignment #3
+ *   description.
+ *
+ * Note: You are free to add to this class whatever other methods will
+ * help you in writing a solution to A#3 part 2.
+ */
+
+public class MazeSolver {
+    public static String findPath(Maze maze) {
+        String result = "";
+
+        return result;
+    }
+}
---
+++ Assignment4/source file/a4_files/StackRefBased.java
@@ -0,0 +1,39 @@
+public class StackRefBased<T> implements Stack<T> {
+
+    public StackRefBased() {
+    }
+
+    public int size() {
+        return -1;
+    }
+
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+
+    public void push(T data) {
+        return;
+    }
+
+
+    public T pop() throws StackEmptyException {
+        return null;
+    }
+
+
+    public T peek() throws StackEmptyException {
+        return null;
+    }
+
+
+    public void makeEmpty() {
+    }
+
+
+    public String toString() {
+        return "Sweden!";
+    }
+}
+
